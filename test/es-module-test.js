2424fe7a59c11f1603670369e6a18b00b794388a
Alan Zhang <gagazcfan@icloud.com>
2016-12-09 14:06:18 +0800

    fix

--- test/es-module-test.js
+++ test/es-module-test.js
@@ -1,7 +1,6 @@
 /// <reference path="../es-module/index.d.ts" />
 
-import Child2 from './child.vue'
+import Child1 from './child.vue'
 
 // access something to generate code
 const d1 = Child1.methods
-const d2 = Child2.data
